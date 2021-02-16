a3c65a4acd6cd1577ac51f2ed3229c16de4df019
teirm <tribunus.vii@gmail.com>
2021-02-16 17:08:21 -0500

    abstracted out hittable things and used this to implement spheres

---
+++ include/sphere.hpp
@@ -0,0 +1,51 @@
+// a hittable sphere class 
+
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.hpp"
+#include "vec3.hpp"
+
+class sphere : public hittable {
+    public:
+        sphere() {}
+        sphere(point3 cen, double r) : center(cen), radius(r) {};
+
+        virtual bool hit(
+            const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+    public:
+        point3 center;
+        double radius;
+};
+
+bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+
+    auto discriminant = half_b * half_b - a*c;
+    if (discriminant < 0) {
+        return false;
+    }
+    auto sqrtd = sqrt(discriminant);
+    
+    // find nearest root that lies in acceptable range
+    // this handles the +/- of the quadratic formula
+    auto root = (-half_b - sqrtd) / a;
+    if (root < t_min || t_max < root) {
+        root = (-half_b + sqrtd) / a;
+        if (root < t_min || t_max < root) {
+            return false;
+        }
+    }
+
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    rec.normal = (rec.p - center) / radius;
+    
+    return true;
+}
+
+#endif /* SPHERE_H */
