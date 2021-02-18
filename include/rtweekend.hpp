8ac6cebae2f75ad890727743f7ea4c5be079da80
teirm <tribunus.vii@gmail.com>
2021-02-17 19:37:42 -0500

    added anti-aliasing

--- include/rtweekend.hpp
+++ include/rtweekend.hpp
@@ -6,6 +6,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 // usings
 
@@ -21,6 +22,23 @@ const double pi = 3.1415926535897932385;
 // utility functions
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    // returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // returns a random real in [min,max)
+    return min + (max-min)*random_double();
+}
+
+// clamp the value of x to range [min,max]
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
 // common headers
